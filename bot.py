61f6e63a6b29b23f486f54ec9ded9a90a2e7ad4c
adilbekzharkyn1988-code <adilbek.zharkyn1988@gmail.com>
2025-08-24 18:10:45 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -46,9 +46,9 @@ async def create_pdf(update: Update, context: ContextTypes.DEFAULT_TYPE):
         pdf = FPDF()
         pdf.add_page()
         pdf.add_font('DejaVu', '', 'DejaVuSans.ttf', uni=True)  # подключаем шрифт
-        pdf.add_font('DejaVu', 'B', 'DejaVuSans-Bold.ttf', uni=True)
+        pdf.add_font('DejaVu', '', 'DejaVuSans-Bold.ttf', uni=True)
         pdf.set_font('DejaVu', '', 12)   # обычный текст
-        pdf.set_font('DejaVu', 'B', 16)
+        pdf.set_font('DejaVu', '', 16)
         pdf.multi_cell(0, 10, title, align="C")
         pdf.ln(5)
 
